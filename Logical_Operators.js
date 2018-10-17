54dc239de6c831fbb13f7b330222332248ec6207
Carina Cruz <lecruz@bu.edu>
2018-10-17 23:21:31 +0000

    updates

--- Logical_Operators.js
+++ Logical_Operators.js
@@ -3,7 +3,7 @@
 let mood = 'sleepy';
 let tirednessLevel = 6;
 
-if (mood === 'sleepy' || tirednessLevel > 8){
+if (mood === 'sleepy' || tirednessLevel > 8){  //or
    console.log('time to sleep');
 } else {
   console.log('not bed time yet')
