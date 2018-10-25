a1a446470afd5ab4687a1f703b85c58a444e040d
Carina Cruz <lecruz@bu.edu>
2018-10-25 22:57:59 +0000

    costOfMonitors

---
+++ costOfMonitors.js
@@ -0,0 +1,12 @@
+//function to figure out cost of monitors
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+function costOfMonitors(rows, columns){
+  return monitorCount(rows, columns) * 200
+}
+
+const totalCost = costOfMonitors(5, 4);
+console.log(totalCost); 
