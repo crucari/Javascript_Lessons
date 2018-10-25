37b9c4b7486c884160cbae3a53b4a1f90d5b7ce4
Carina Cruz <lecruz@bu.edu>
2018-10-25 00:48:35 +0000

    string concatenation + adding name

--- sayThanks.js
+++ sayThanks.js
@@ -1,3 +1,5 @@
-function sayThanks() {
-  console.log('Thank you for your purchase! We appreciate your business.');
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
 }
+
+sayThanks('Cole');
