56fe31c75dc058a71d40b75e0d71cbba9c2da732
Carina Cruz <lecruz@bu.edu>
2018-11-03 03:02:29 +0000

    working with const and let

---
+++ utensils.js
@@ -0,0 +1,25 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+// Below the two existing arrays, re-assign the element in index 0 of condiments to 'Mayo'.
+
+// Log the updated array, condiments, to the console.
+
+condiments[0] = 'Mayo';
+
+console.log(condiments);
+
+// Below your code from Step 1, reassign condiments to be a new array that contains a single string ['Mayo']
+
+// Log the result to the console.
+
+condiments = ['Mayo'];
+
+// Below your code from Step 2, re-assign the last item from the utensils array to 'Spoon'.
+
+utensils[3] = 'Spoon';
+
+console.log(utensils);
+
+// utensils = ['Spork'];
