a9a2a586344c0e6683fb4baf272de110bc13c6a7
Carina Cruz <lecruz@bu.edu>
2018-11-11 04:06:00 +0000

    do while practice

--- cupsSugar.js
+++ cupsSugar.js
@@ -1,3 +1,11 @@
-const cupsOfSugarNeeded = (4); 
-const cupsAdded = (0);
+// Add at least one cup of sugar to the batter even if the value of cupsOfSugarNeeded is 0.
+// Create a do...while loop which increments cupsAdded by one 
+// while cupsAdded is less than cupsOfSugarNeeded.
+
+let cupsOfSugarNeeded = 3;
+let cupsAdded = 0;
+
+do {
+ cupsAdded++
+} while (cupsAdded < cupsOfSugarNeeded);
 
