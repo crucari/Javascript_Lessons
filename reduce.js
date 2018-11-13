ae98d570fd8229d249a9484da294f677895795b4
Carina Cruz <lecruz@bu.edu>
2018-11-13 23:51:12 +0000

    reduce.js

---
+++ reduce.js
@@ -0,0 +1,6 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+});
+
