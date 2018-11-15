f1f11f4b22a1802e1f4f03ab312cd9ef888eb1e0
Carina Cruz <lecruz@bu.edu>
2018-11-15 23:55:02 +0000

    cities.js

---
+++ cities.js
@@ -0,0 +1,10 @@
+const cities = ['Orlando', 'Dubai', 'Edinburgh', 'Chennai', 'Accra', 'Denver', 'Eskisehir', 'Medellin', 'Yokohama'];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.method(city => console.log('Have you visited ' + city + '?'));
+
+// Choose a method that will return a new array
+const longCities = cities.method(city => city.length > 7);
+
