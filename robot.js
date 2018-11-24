c50110c5ea027de4f50e8d72cb07524f1e799465
Carina Cruz <lecruz@bu.edu>
2018-11-24 04:59:22 +0000

    edit

--- robot.js
+++ robot.js
@@ -2,7 +2,7 @@ const robot = {
   model: '1E78V2',
   energyLevel: 100,
   provideInfo() { 
-    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.` //I am this model and my current energy level is
   }
 };
 
