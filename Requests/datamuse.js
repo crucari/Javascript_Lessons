f1d037642b11e6966bae098777930ce5e76b74a9
Carina Cruz <lecruz@bu.edu>
2018-11-28 00:24:44 +0000

    updated datamuse

--- Requests/datamuse.js
+++ Requests/datamuse.js
@@ -1,5 +1,6 @@
 // Information to reach API
-
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
 
 // Selecting page elements
 const inputField = document.querySelector('#input');
@@ -8,7 +9,20 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
+  
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
 
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      renderResponse(xhr.response);
+    }
+  }
+  
+  xhr.open('GET', endpoint);
+  xhr.send();
 }
 
 // Clear previous results and display results to webpage
@@ -16,7 +30,7 @@ const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild);
-  };
+  }
   getSuggestions();
 }
 
